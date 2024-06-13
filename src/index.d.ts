16a35fe2ea7294fde89473803c8e16ed1b99bb25
Shubham Tiwari <shubht3303@gmail.com>
2024-06-13 12:43:41 +0530

    some corrections

---
+++ src/index.d.ts
@@ -0,0 +1,13 @@
+interface BadWordsDictionary {
+    [key: string]: number;
+}
+declare const cleanedMessage: {
+    maskBadWords: (message: string, maskWith?: string) => string;
+    isMessageDirty: (message: string) => boolean;
+    addWords: (wordList: string | string[]) => BadWordsDictionary;
+    removeWords: (wordList: string | string[]) => BadWordsDictionary;
+    countBadWords: (message: string) => number;
+    getListOfBadWords: (message: string) => string[];
+    isSpecificWordBad: (word: string) => boolean;
+};
+export = cleanedMessage;
